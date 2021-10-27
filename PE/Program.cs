0a84689e4efde41afb136e6b2c6ee894416bc8a4
Cxing <cheneychenstar@gmail.com>
2021-10-27 14:29:18 +0800

    update

---
+++ PE/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using PEParser;
+
+namespace PE
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var pe = new PEFile(@"C:\Users\cxing\Desktop\ctf3.exe");
+            //var el = (int)pe.PeParser.ReadDword(0x3c);
+            
+            
+            return;
+        }
+    }
+    
+
+    
+
+}
